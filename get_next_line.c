5044a8299c33cd238b28175b5b047e32bc0895d2
Ali Gökçek <agokcek@k1m26s06.42istanbul.com.tr>
2024-12-27 18:23:18 +0300

    :)

--- get_next_line.c
+++ get_next_line.c
@@ -1,114 +1,69 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: agokcek <agokcek@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/21 16:08:21 by agokcek           #+#    #+#             */
-/*   Updated: 2024/12/22 17:39:39 by agokcek          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "get_next_line.h"
 
-#include "get_next_line.h"
-
-static char	*after_nl(char *s, char c)
+/* char *get_next_line(int fd)
 {
-	char	*str;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	if (!s[i])
-	{
-		free(s);
-		return (NULL);
-	}
-	while (s[i] && s[i] != c)
-		i++;
-	str = (char *)malloc((ft_strlen(s) - i) + 1);
-	if (!str)
-	{
-		free(s);
-		return (NULL);
-	}
-	i++;
-	while (s[i])
-		str[j++] = s[i++];
-	str[j] = '\0';
-	free(s);
-	return (str);
+    
 }
+*/
 
-static char	*before_nl(char *s, char c)
+int main()
 {
-	char	*str;
-	int		i;
+    char *s;
+    char *str;
+    char *line;
+    int fd;
+    int i;
+    int j;
+    char * n_position;
+    
+    i = 1;
+    j = 0;
+    fd = open("Mustafam.txt",O_RDWR, 0777);
+    s = (char *)malloc(BUFFER_SIZE+1);
+    str = malloc(1);
+    line = malloc(1);
 
-	i = 0;
-	if (!s[i])
-		return (NULL);
-	while (s[i] != '\0' && s[i] != c)
-		i++;
-	str = (char *)malloc(sizeof(char) * (i + 2));
-	if (!str)
-	{
-		free(str);
-		return (NULL);
-	}
-	i = 0;
-	while (s[i] && s[i] != c)
-	{
-		str[i] = s[i];
-		i++;
-	}
-	if (s[i] == c)
-	{
-		str[i] = s[i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
+    while(i > 0)
+    {
+        i = read(fd,s,BUFFER_SIZE);
+        s[i] = '\0';
+        n_position= ft_strchr(s,'\n');
+        if (!n_position)
+        {
+            // n position dahil olacak şekilde bir yere kopyala, bu senin return edeceğin line'ın
+            // n positiondan sonraki kısım da remainder ın, bunu static variableda store etmen gerekiyor ki sonraki fonkisyon çağırılışında bunu da dahil etsin
+        }
+    
+    }
+    
+    while(str[j] != '\n' || str[j] != '\0')
+    {
+        if (str[j]=='\n')
+        {
+            // \n i dahil etmek için burayı yazabilirsin
+        }
+        
+        line[j] = str[j];
+        j++;
+    }
 
-static char	*next_line(int fd, char *s)
-{
-	char	*buf;
-	int		i;
+    // printf("%s\n",line);
 
-	buf = (char *)malloc(sizeof(char) * (size_t)BUFFER_SIZE + 1);
-	if (!buf)
-		return (NULL);
-	i = 1;
-	while (!ft_strchr(s, '\n') && i != 0)
-	{
-		i = read(fd, buf, BUFFER_SIZE);
-		if (i == -1)
-		{
-			free(buf);
-			return (NULL);
-		}
-		buf[i] = '\0';
-		s = ft_strjoin(s, buf);
-	}
-	free(buf);
-	return (s);
-}
+    // str=str+j+1;
+    // j=0;
 
-char	*get_next_line(int fd)
-{
-	char		*str;
-	static char	*line;
+    // while(str[j] != '\n')
+    // {
+    //     line[j] = str[j];
+    //     j++;
+    // }
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	line = next_line(fd, line);
-	if (line)
-	{
-		str = before_nl(line, '\n');
-		line = after_nl(line, '\n');
-		return (str);
-	}
-	return (NULL);
+    printf("%s\n",str);
+
+
+
 }
